44f5be34090b06bab62ac5fe8d6ce3a9f5a1d02b
Paylak08 <paylak.jeyranyan@tumo.org>
2023-12-04 17:23:50 +0400

    lesson 3, game

--- finishingpro/nss.js
+++ finishingpro/nss.js
@@ -1,7 +1,6 @@
-class Nss{
-    constructor(x,y){
-        this.x = x
-        this.y = y
+class Nss extends LivigCreature{
+    constructor(x, y){
+        super(x, y)
         this.directions = []
     }
     getNewCoordinates(){
@@ -15,28 +14,6 @@ class Nss{
             [this.x , this.y + 1],
             [this.x + 1, this.y + 1]
             ];
-    }
-    chooseCell(char1, char2){
-        this.getNewCoordinates()
-        let found = []
-        for(let i in this.directions){
-            let x = this.directions[i][0]
-            let y = this.directions[i][1]
-    
-            if(x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length){
-                if (matrix[y][x] == char1){
-                    found.push(this.directions[i])
-                }
-
-            }
-            if(x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length){
-                if (matrix[y][x] == char2){
-                    found.push(this.directions[i])
-                }
-
-            }
-        }
-        return found
     }
     // move(){
     //     //let emptyCells = this.x + this.y
--- finishingpro/superRob.js
+++ finishingpro/superRob.js
@@ -1,7 +1,6 @@
-class SuperRobber{
-    constructor (x,y){
-        this.x = x
-        this.y = y
+class SuperRobber extends LivigCreature{
+    constructor (x, y){
+        super(x, y)
         this.energy = 30
         this.directions = []
     }
@@ -17,22 +16,9 @@ class SuperRobber{
             [this.x + 1, this.y + 1]
             ];
     }
-    chooseCell(char){
+    chooseCell(char1){
         this.getNewCoordinates()
-        let found = []
-    
-        for(let i in this.directions){
-            let x = this.directions[i][0]
-            let y = this.directions[i][1]
-    
-            if(x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length){
-                if (matrix[y][x] == char){
-                    found.push(this.directions[i])
-                }
-            }
-        }
-    
-        return found
+        return super.chooseCell(char1)
     }
 
     mul(){
