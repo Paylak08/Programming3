02479f68128980d6c0eaa7698d0ab240dae1a22e
Paylak08 <paylak.jeyranyan@tumo.org>
2023-12-08 17:16:13 +0400

    Lesson4, tnayind

---
+++ finishingpro/event4/scriptt.js
@@ -0,0 +1,14 @@
+// let clickCount = 0;
+// function clickHandler(evt) {
+//     clickCount++;
+//     console.log(evt);
+//     let str = "Thanks for clicking " + clickCount;
+//     this.innerText = str;
+// }
+
+// let p = document.getElementById("pElement");
+// p.addEventListener("mouseover", clickHandler);
+function bodyClick(evt){
+    console.log("Clicked at X: " + evt.pageX + ", Y: " + evt.pageY);
+    }
+    window.onclick = bodyClick;
