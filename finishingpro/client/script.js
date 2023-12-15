7c2fb914f3365139d24fc5093f6ede9feacb6932
Paylak08 <paylak.jeyranyan@tumo.org>
2023-12-15 17:20:49 +0400

    lesson 7,finish3

--- finishingpro/client/script.js
+++ finishingpro/client/script.js
@@ -1,100 +1,25 @@
-    function matrixGenerator(matrixSize, goldCount, robberCount, policeCount, NSSCount, supRobCount) {
-    let matrix = []
-    for (let i = 0; i < matrixSize; i++) {
-        matrix.push([])
-        for (let j = 0; j < matrixSize; j++) {
-            matrix[i].push(0)
-        }
-    }
-    for (let i = 0; i < goldCount; i++) {
-        let x = Math.floor(Math.random() * matrixSize)
-        let y = Math.floor(Math.random() * matrixSize)
-        if (matrix[y][x] == 0) {
-            matrix[y][x] = 1
-        }
+var socket = io();
 
-    }
-    for (let i = 0; i < robberCount; i++) {
-        let x = Math.floor(Math.random() * matrixSize)
-        let y = Math.floor(Math.random() * matrixSize)
+side = 20
 
-        if (matrix[y][x] == 0) {
-            matrix[y][x] = 2
-        }
-
-    }
-    for (let i = 0; i < policeCount; i++) {
-        let x = Math.floor(Math.random() * matrixSize)
-        let y = Math.floor(Math.random() * matrixSize)
-
-        if (matrix[y][x] == 0) {
-            matrix[y][x] = 3
-        }
-    }
-    for (let i = 0; i < matrixSize; i++) {
-
-        for (let j = 0; j < matrixSize; j++) {
-            if (i == j){//0 && j == 0) {
-                matrix[i][j] = 4
-            }// } else if (i == 0 && j == matrixSize-1) {
-            //     matrix[i][j] = 4
-            // }
-        }
-    }
-    return matrix
-}
-let matrix = matrixGenerator(50, 75, 20, 40, 2)
-let side = 17
-let goldArr = []
-let robberArr = []
-let policeArr = []
-let NSSArr = []
-let supRobArr = []
 function setup() {
     frameRate(10)
-    createCanvas(matrix[0].length * side, matrix.length * side)
-    for (let y = 0; y < matrix.length; y++) {
-        for (let x = 0; x < matrix[y].length; x++) {
-            if (matrix[y][x] == 1) {
-                let gold = new Gold(x, y)
-                goldArr.push(gold)
-            } else if (matrix[y][x] == 2) {
-                let robber = new Robber(x, y)
-                robberArr.push(robber)
-            } else if (matrix[y][x] == 3) {
-                let pred = new Police(x, y)
-                policeArr.push(pred)
-            } else if (matrix[y][x] == 4) {
-                let nss = new Nss(x, y)
-                NSSArr.push(nss)
-            }else if (matrix[y][x] == 5) {
-                let rob = new SuperRobber(x, y)
-                supRobArr.push(rob)
-            }
-        }
+    createCanvas(50 * side, 50 * side)
+}
+
+function addGold(){
+    socket.emit("add gold")
+}
+
+function SupRob(x,y){
+    let obj = {
+        x,
+        y
     }
+    socket.emit("SuperRobber", JSON.stringify(obj))
 }
-function one(){
-    for (let i = 0; i < matrix.length; i++) {
-        for (let j = 0; j < matrix[i].length; j++) {
-            if(matrix[i][j] == 1){
-                matrix[i][j] = 0
-                for(let i in goldArr){
-                    goldArr.splice(i,goldArr.length)
-                    break;
-                }
-            }
-        }
-    }
-}
-function two(x,y){
-        if (matrix[y][x] == 0) {
-            matrix[y][x] = 5
-            let rob = new SuperRobber(x, y)
-                supRobArr.push(rob)
-        }
-}
-function draw() {
+
+function nkarel(matrix) {
     for (let y = 0; y < matrix.length; y++) {
         for (let x = 0; x < matrix[y].length; x++) {
             if (matrix[y][x] == 1) {
@@ -105,7 +30,7 @@ function draw() {
                 fill("blue")
             } else if (matrix[y][x] == 4) {
                 fill("red")
-            }else if (matrix[y][x] == 5) {
+            } else if (matrix[y][x] == 5) {
                 fill("green")
             } else {
                 fill("gray")
@@ -115,48 +40,13 @@ function draw() {
             rect(x * side, y * side, side, side)
         }
     }
-
-    for (let i in goldArr) {
-        goldArr[i].mul()
-    }
-    for (let i in robberArr) {
-        robberArr[i].eat()
-    }
-    for (let i in policeArr) {
-        policeArr[i].eat()
-    }
-    for (let i in NSSArr) {
-        NSSArr[i].eat()
-    }
-    for (let i in supRobArr) {
-        supRobArr[i].eat()
-    }
-    if (goldArr.length == 0) {
-        let x1 = Math.floor(Math.random() * matrix.length)
-        let y1 = Math.floor(Math.random() * matrix.length)
-        if (matrix[y1][x1] != 2 && matrix[y1][x1] != 3) {
-            matrix[y1][x1] = 1
-            let gold = new Gold(x1, y1)
-            goldArr.push(gold)
-        }
-    }
-    if (robberArr.length == 0) {
-        let x1 = Math.floor(Math.random() * matrix.length)
-        let y1 = Math.floor(Math.random() * matrix.length)
-        if (matrix[y1][x1] != 3) {
-            matrix[y1][x1] = 2
-            let robber = new Robber(x1, y1)
-            robberArr.push(robber)
-        }
-    }
-    if (policeArr.length == 0) {
-        let x1 = Math.floor(Math.random() * matrix.length)
-        let y1 = Math.floor(Math.random() * matrix.length)
-        matrix[y1][x1] = 3
-        let p = new Police(x1, y1)
-        policeArr.push(p)
-    }
 }
 function mouseClicked() {
     console.log(mouseX, mouseY);
-    }
+}
+
+setInterval(
+    function () {
+        socket.on('send matrix', nkarel)
+    }, 1000
+)
--- finishingpro/server/server.js
+++ finishingpro/server/server.js
@@ -1,45 +1,177 @@
-// let express = require("express");
-// let square = require("../square")
-// let app = express();
-// let a = new square(2)
-// console.log(a.countSquare())
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+var fs = require("fs");
 
-// app.use(express.static("."));
+app.use(express.static("../client"));
 
-// app.get("/", function(req, res){
-// res.redirect("index.html");
-// });
+app.get('/', function (req, res) {
+    res.redirect('index.html');
+});
+server.listen(3000, () => {
+    console.log('connected');
+});
 
-// app.listen(3000, function(){
-// console.log("Example is running on port 3000");
-// });
-// let fs = require('fs');
+function matrixGenerator(matrixSize, goldCount, robberCount, policeCount, NSSCount, supRobCount) {
+    let matrix = []
+    for (let i = 0; i < matrixSize; i++) {
+        matrix.push([])
+        for (let j = 0; j < matrixSize; j++) {
+            matrix[i].push(0)
+        }
+    }
+    for (let i = 0; i < goldCount; i++) {
+        let x = Math.floor(Math.random() * matrixSize)
+        let y = Math.floor(Math.random() * matrixSize)
+        if (matrix[y][x] == 0) {
+            matrix[y][x] = 1
+        }
 
-// function main() {
-//     let file = "Paylak.txt";
+    }
+    for (let i = 0; i < robberCount; i++) {
+        let x = Math.floor(Math.random() * matrixSize)
+        let y = Math.floor(Math.random() * matrixSize)
 
-//     fs.appendFileSync(file, "Hello world\n");
-// }
-// main();
-// let fs = require('fs');
-// function main() {
-//     fs.writeFile("hello.txt", "Hello world\n", function (err) {
-//         console.log("fs.writeFile ended");
-//     });
-//     console.log("fs.writeFile");
-// }
-// main();
-let obj = {
-    "first_name": "Vardan",
-    "last_name": "Hovsepyan",
-    "age": 13,
-    "tumo_student": true 
+        if (matrix[y][x] == 0) {
+            matrix[y][x] = 2
+        }
+
+    }
+    for (let i = 0; i < policeCount; i++) {
+        let x = Math.floor(Math.random() * matrixSize)
+        let y = Math.floor(Math.random() * matrixSize)
+
+        if (matrix[y][x] == 0) {
+            matrix[y][x] = 3
+        }
+    }
+    for (let i = 0; i < matrixSize; i++) {
+
+        for (let j = 0; j < matrixSize; j++) {
+            if (i == j) {
+                matrix[i][j] = 4
+            }
+        }
+    }
+    return matrix
 }
-let name = obj.name
-let last_name = obj.last_name
-let age = obj.age
-let tumo = obj.tumo_student
-JSON.stringify(name)
-JSON.stringify(last_name)
-JSON.stringify(age)
-JSON.stringify(tumo_student)
+
+matrix = matrixGenerator(50, 75, 20, 40, 2)
+
+io.sockets.emit('send matrix', matrix)
+
+goldArr = []
+robberArr = []
+policeArr = []
+NSSArr = []
+supRobArr = []
+
+Gold = require('./gold')
+Robber = require('./robber')
+SuperRobber = require('./superRob')
+Police = require('./police')
+Nss = require('./nss')
+
+function one() {
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j < matrix[i].length; j++) {
+            if (matrix[i][j] == 1) {
+                matrix[i][j] = 0
+                for (let i in goldArr) {
+                    goldArr.splice(i, goldArr.length)
+                    break;
+                }
+            }
+        }
+    }
+}
+
+function two(obj) {
+    let cord = JSON.parse(obj);
+    let x = cord.x;
+    let y = cord.y
+    if (matrix[y][x] == 0) {
+        matrix[y][x] = 5
+        let rob = new SuperRobber(x, y)
+        supRobArr.push(rob)
+    }
+}
+
+function object(matrix){
+    for (let y = 0; y < matrix.length; y++) {
+        for (let x = 0; x < matrix[y].length; x++) {
+            if (matrix[y][x] == 1) {
+                let gold = new Gold(x, y)
+                goldArr.push(gold)
+            } else if (matrix[y][x] == 2) {
+                let robber = new Robber(x, y)
+                robberArr.push(robber)
+            } else if (matrix[y][x] == 3) {
+                let pred = new Police(x, y)
+                policeArr.push(pred)
+            } else if (matrix[y][x] == 4) {
+                let nss = new Nss(x, y)
+                NSSArr.push(nss)
+            } else if (matrix[y][x] == 5) {
+                let rob = new SuperRobber(x, y)
+                supRobArr.push(rob)
+            }
+        }
+    }
+
+    io.sockets.emit('send matrix', matrix)
+}
+
+function game(){
+    for (let i in goldArr) {
+        goldArr[i].mul()
+    }
+    for (let i in robberArr) {
+        robberArr[i].eat()
+    }
+    for (let i in policeArr) {
+        policeArr[i].eat()
+    }
+    for (let i in NSSArr) {
+        NSSArr[i].eat()
+    }
+    for (let i in supRobArr) {
+        supRobArr[i].eat()
+    }
+    if (goldArr.length == 0) {
+        let x1 = Math.floor(Math.random() * matrix.length)
+        let y1 = Math.floor(Math.random() * matrix.length)
+        if (matrix[y1][x1] != 2 && matrix[y1][x1] != 3) {
+            matrix[y1][x1] = 1
+            let gold = new Gold(x1, y1)
+            goldArr.push(gold)
+        }
+    }
+    if (robberArr.length == 0) {
+        let x1 = Math.floor(Math.random() * matrix.length)
+        let y1 = Math.floor(Math.random() * matrix.length)
+        if (matrix[y1][x1] != 3) {
+            matrix[y1][x1] = 2
+            let robber = new Robber(x1, y1)
+            robberArr.push(robber)
+        }
+    }
+    if (policeArr.length == 0) {
+        let x1 = Math.floor(Math.random() * matrix.length)
+        let y1 = Math.floor(Math.random() * matrix.length)
+        matrix[y1][x1] = 3
+        let p = new Police(x1, y1)
+        policeArr.push(p)
+    }
+
+    io.sockets.emit('send matrix', matrix)
+}
+
+setInterval(game, 1000)
+
+io.on('connection', function(socket){
+    object(matrix);
+    socket.on("add gold", one)
+    socket.on("SuperRobber", two)
+})
